d73968b4d6c643afb76148e8af95c92b3ad99b8d
chaseltb <chasethethompson@icloud.com>
2025-09-08 12:54:31 -0400

    user mapper, jdbc repo, and repo interface

--- lava-donut/src/main/java/learn/lavadonut/data/UserRepository.java
+++ lava-donut/src/main/java/learn/lavadonut/data/UserRepository.java
@@ -1,5 +1,17 @@
 package learn.lavadonut.data;
 
-public class UserRepository {
+import learn.lavadonut.models.User;
 
+import java.util.List;
+
+public interface UserRepository {
+    List<User> findAll();
+
+    User findByUsername(String username);
+
+    User add(User user);
+
+    boolean update(User user);
+
+    boolean deleteById(int userId);
 }
