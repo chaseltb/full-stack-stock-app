e5b249fd3dad852235a5df8083a6cee725cb0527
stokesm5 <stokesm5@tcnj.edu>
2025-09-08 12:06:27 -0400

    countries table implemented

--- lava-donut/db/stock-app-schema.sql
+++ lava-donut/db/stock-app-schema.sql
@@ -14,5 +14,16 @@ create table stock_exchange (
 	stock_exchange_id int primary key auto_increment,
     `name` varchar(125) not null, -- EX: New York Stock Exchange
     `code` varchar(12) not null, -- EX: NYSE
-	timezone int not null -- EX: -- EX: -5
+	timezone varchar(12) not null -- EX: -5
 ); 
+
+-- https://www.isin.net/country-codes/
+create table countries (
+	country_id int primary key auto_increment,
+    `name` varchar(125) not null, -- EX: United States of America
+    `code` varchar(4) not null, -- EX: US
+    currency_id int not null,
+    constraint fk_countries_currency_id
+		foreign key (currency_id)
+        references currencies(currency_id)
+);
