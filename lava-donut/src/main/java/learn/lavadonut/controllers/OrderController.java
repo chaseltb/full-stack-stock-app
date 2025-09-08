27cb502494dd70d093519754de0533b9551b9267
tomiwaadewolu <tomi.aj91@gmail.com>
2025-09-08 14:03:44 -0500

    Stub out controller for order

--- lava-donut/src/main/java/learn/lavadonut/controllers/OrderController.java
+++ lava-donut/src/main/java/learn/lavadonut/controllers/OrderController.java
@@ -1,4 +1,17 @@
 package learn.lavadonut.controllers;
 
+import learn.lavadonut.domain.OrderService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Spring DI and MVC
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/order") // Base URL
 public class OrderController {
+    private OrderService service;
+
+    public OrderController(OrderService service) {
+        this.service = service;
+    }
 }
