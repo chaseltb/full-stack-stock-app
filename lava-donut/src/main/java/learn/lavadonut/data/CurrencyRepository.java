1c691ea42ac912c65206e733f633b0eb31af97e3
stokesm5 <stokesm5@tcnj.edu>
2025-09-08 17:44:27 -0400

    Currency interface methods implemented

--- lava-donut/src/main/java/learn/lavadonut/data/CurrencyRepository.java
+++ lava-donut/src/main/java/learn/lavadonut/data/CurrencyRepository.java
@@ -1,4 +1,18 @@
 package learn.lavadonut.data;
 
+import learn.lavadonut.models.Currency;
+
+import java.util.List;
+
 public interface CurrencyRepository {
+
+    List<Currency> findAll();
+
+    Currency findById(int currencyId);
+
+    Currency add (Currency currency);
+
+    boolean update (Currency currency);
+
+    boolean delete (int currencyId);
 }
