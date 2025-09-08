6bf88e0b0e5c1d342e5a3683664c584cdfef3de2
stokesm5 <stokesm5@tcnj.edu>
2025-09-08 11:57:04 -0400

    currencies and stock_exchange table complete

--- lava-donut/db/stock-app-schema.sql
+++ lava-donut/db/stock-app-schema.sql
@@ -5,5 +5,14 @@ use stock_schema;
 -- create tables and table relationships in accordance to ERD.png in root file
 create table currencies (
 	currency_id int primary key auto_increment,
-    `name` varchar
+    `name` varchar(50) not null, -- EX: Euros
+    `code` varchar(12) not null,  -- EX: ISO 4217  
+    value_to_usd decimal not null -- EX: 1.17
 );
+
+create table stock_exchange (
+	stock_exchange_id int primary key auto_increment,
+    `name` varchar(125) not null, -- EX: New York Stock Exchange
+    `code` varchar(12) not null, -- EX: NYSE
+	timezone int not null -- EX: -- EX: -5
+); 
