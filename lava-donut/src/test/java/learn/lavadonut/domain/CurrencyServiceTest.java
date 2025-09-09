060704c5fe4d9844357180eb6807a7ff96a7fdab
stokesm5 <stokesm5@tcnj.edu>
2025-09-09 15:45:21 -0400

    update happy and unhappy path tests implemented

--- lava-donut/src/test/java/learn/lavadonut/domain/CurrencyServiceTest.java
+++ lava-donut/src/test/java/learn/lavadonut/domain/CurrencyServiceTest.java
@@ -138,7 +138,6 @@ class CurrencyServiceTest {
         assertEquals(ResultType.INVALID, result.getType());
     }
 
-
     @Test
     void shouldNotAddWhenIdIsGreaterThanZero(){
         Currency arg = makeCurrency();
@@ -147,6 +146,109 @@ class CurrencyServiceTest {
 
         assertEquals(ResultType.INVALID, result.getType());
     }
+
+    /**
+     update Tests!
+     **/
+
+    @Test
+    void shouldUpdateWhenValid(){ // HAPPY PATH
+        Currency currency = makeCurrency();
+        currency.setName("Alternate Yen");
+
+        when(repository.update(currency)).thenReturn(true);
+        Result<Currency> result = service.update(currency);
+        assertEquals(ResultType.SUCCESS, result.getType());
+    }
+
+    @Test
+    void shouldNotUpdateWhenCurrencyNull(){ //UNHAPPY PATH
+        Result<Currency> result = service.update(null);
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldNotUpdateWhenCurrencyNameIsNull(){
+        Currency arg = makeCurrency();
+        arg.setName(null);
+
+        Result<Currency> result = service.update(arg);
+
+        assertEquals(ResultType.INVALID, result.getType());
+
+        arg.setName("");
+
+        Result<Currency> blankResult = service.update(arg);
+
+        assertEquals(ResultType.INVALID, blankResult.getType());
+
+    }
+
+    @Test
+    void shouldNotUpdateWhenCurrencyCodeIsNull(){
+        Currency arg = makeCurrency();
+        arg.setCode(null);
+
+        Result<Currency> result = service.update(arg);
+
+        assertEquals(ResultType.INVALID, result.getType());
+
+        arg.setCode("");
+
+        Result<Currency> blankResult = service.update(arg);
+
+        assertEquals(ResultType.INVALID, blankResult.getType());
+    }
+
+    @Test
+    void shouldNotUpdateWhenCurrencyValueIsNotGreaterThanZero(){
+        Currency arg = makeCurrency();
+        arg.setValueToUsd(BigDecimal.ZERO);
+
+        Result<Currency> result = service.update(arg);
+
+        assertEquals(ResultType.INVALID, result.getType());
+
+        arg.setValueToUsd(BigDecimal.valueOf(-1));
+
+        Result<Currency> negativeResult = service.update(arg);
+
+        assertEquals(ResultType.INVALID, negativeResult.getType());
+    }
+
+    @Test
+    void shouldNotUpdateDuplicate(){
+        Currency currency = makeCurrency();
+        List<Currency> yen = List.of(makeCurrency());
+
+        when(repository.findAll()).thenReturn(yen);
+
+        Result<Currency> result = service.update(currency);
+
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldNotUpdateWhenIdIsLessThanOne(){
+        Currency arg = makeCurrency();
+        arg.setId(0);
+
+        Result<Currency> result = service.update(arg);
+
+        assertEquals(ResultType.INVALID, result.getType());
+    }
+
+    @Test
+    void shouldNotUpdateNonExistentCurrency(){
+        Currency arg = makeCurrency();
+
+        when(repository.update(arg)).thenReturn(false);
+
+        Result<Currency> result = service.update(arg);
+
+        assertEquals(ResultType.NOT_FOUND, result.getType());
+    }
+
     private List<Currency> makeCurrencyList(){
         // 1: ('United States dollar', 'USD', 1.0)
         // 2: (2, 'Euro', 'EUR', 1.17)
