64106df69f5a9bfe6df4a84ddda3f103471dd8ac
stokesm5 <stokesm5@tcnj.edu>
2025-09-09 16:31:55 -0400

    StockRepository interface implemented, StockController added to file structure

---
+++ lava-donut/src/main/java/learn/lavadonut/data/StockRepository.java
@@ -0,0 +1,21 @@
+package learn.lavadonut.data;
+
+import learn.lavadonut.models.Stock;
+
+import java.util.List;
+
+public interface StockRepository {
+    List<Stock> findAll();
+
+    List<Stock> getStocksByIndustry(String industry);
+
+    Stock findById(int stockId);
+
+    Stock findByTicker(String ticker);
+
+    Stock add(Stock stock);
+
+    boolean update(Stock stock);
+
+    boolean deleteById(int stockId);
+}
