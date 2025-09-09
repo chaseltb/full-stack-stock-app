fbdbc200d7ae6852fba4a20911f7e0e4d0bf22ba
stokesm5 <stokesm5@tcnj.edu>
2025-09-09 19:47:35 -0400

    validate method implemented

--- lava-donut/src/main/java/learn/lavadonut/domain/StockService.java
+++ lava-donut/src/main/java/learn/lavadonut/domain/StockService.java
@@ -1,9 +1,11 @@
 package learn.lavadonut.domain;
 
 import learn.lavadonut.data.StockRepository;
+import learn.lavadonut.models.AssetType;
 import learn.lavadonut.models.Stock;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -37,8 +39,29 @@ public class StockService {
             return result;
         }
 
+        if(Validations.isNullOrBlank(stock.getName())){
+            result.addMessage("stock must have a name!", ResultType.INVALID);
+        }
+        if(Validations.isNullOrBlank(stock.getTicker())){
+            result.addMessage("stock must have a ticker!", ResultType.INVALID);
+        }
 
+        if(stock.getStockExchange().getId() <= 0){
+            result.addMessage("stock must have a listed Stock Exchange!", ResultType.INVALID);
+        }
 
-        return null;
+        if(stock.getCountry().getId() <= 0){
+            result.addMessage("stock must have a listed Country!", ResultType.INVALID);
+        }
+
+        List<Stock> allStock = repository.findAll();
+
+        for (Stock s : allStock){
+            if(s.equals(stock)){
+                result.addMessage("duplicate stocks are not allowed!", ResultType.INVALID);
+            }
+        }
+
+        return result;
     }
 }
