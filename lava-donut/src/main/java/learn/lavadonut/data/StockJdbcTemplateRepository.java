f7f583f3080b4934bab629242498711361b28fb9
stokesm5 <stokesm5@tcnj.edu>
2025-09-09 17:34:26 -0400

    method sql statements redone to inner join and get child data

--- lava-donut/src/main/java/learn/lavadonut/data/StockJdbcTemplateRepository.java
+++ lava-donut/src/main/java/learn/lavadonut/data/StockJdbcTemplateRepository.java
@@ -14,24 +14,44 @@ public class StockJdbcTemplateRepository implements StockRepository{
     //TODO: IF ERROR, RE-EXAMINE STOCK MAPPER
     @Override
     public List<Stock> findAll() {
-        final String sql = "select stock_id, `name`, `ticker`, asset_type, industry, stock_exchange_id, country_id "
-                + "from stocks limit 1000;";
+        final String sql = "select s.stock_id, s.`name`, s.`ticker`, s.asset_type, s.industry, "
+                + "se.stock_exchange_id, se.`name`, se.`code`, se.timezone, "
+                + "c.country_id, c.`name`, c.`code`, "
+                + "cu.currency_id, cu.`name`, cu.`code`, cu.value_to_usd "
+                + "from stock_exchange se "
+                + "inner join stocks s on se.stock_exchange_id = s.stock_exchange_id "
+                + "inner join countries c on s.country_id = c.country_id "
+                + "inner join currencies cu on c.currency_id = cu.currency_id "
+                + "limit 1000;";
         return jdbcTemplate.query(sql, new StockMapper());
     }
 
     @Override
     public List<Stock> getStocksByIndustry(String industry) {
-        final String sql = "select stock_id, `name`, `ticker`, asset_type, industry, stock_exchange_id, country_id "
-                + "from stocks "
-                + "where industry = ? limit 1000;";
+        final String sql = "select s.stock_id, s.`name`, s.`ticker`, s.asset_type, s.industry, "
+                + "se.stock_exchange_id, se.`name`, se.`code`, se.timezone, "
+                + "c.country_id, c.`name`, c.`code`, "
+                + "cu.currency_id, cu.`name`, cu.`code`, cu.value_to_usd "
+                + "from stock_exchange se "
+                + "inner join stocks s on se.stock_exchange_id = s.stock_exchange_id "
+                + "inner join countries c on s.country_id = c.country_id "
+                + "inner join currencies cu on c.currency_id = cu.currency_id "
+                + " where s.industry = ? "
+                + "limit 1000;";
         return jdbcTemplate.query(sql, new StockMapper());
     }
 
     @Override
     public Stock findById(int stockId) {
-        final String sql = "select stock_id, `name`, `ticker`, asset_type, industry, stock_exchange_id, country_id "
-                + "from stocks "
-                + "where stock_id = ?;";
+        final String sql = "select s.stock_id, s.`name`, s.`ticker`, s.asset_type, s.industry, "
+                + "se.stock_exchange_id, se.`name`, se.`code`, se.timezone, "
+                + "c.country_id, c.`name`, c.`code`, "
+                + "cu.currency_id, cu.`name`, cu.`code`, cu.value_to_usd "
+                + "from stock_exchange se "
+                + "inner join stocks s on se.stock_exchange_id = s.stock_exchange_id "
+                + "inner join countries c on s.country_id = c.country_id "
+                + "inner join currencies cu on c.currency_id = cu.currency_id "
+                + " where s.stock_id = ?;";
 
         return jdbcTemplate.query(sql, new StockMapper(), stockId)
                 .stream()
@@ -40,9 +60,15 @@ public class StockJdbcTemplateRepository implements StockRepository{
 
     @Override
     public Stock findByTicker(String ticker) {
-        final String sql = "select stock_id, `name`, `ticker`, asset_type, industry, stock_exchange_id, country_id "
-                + "from stocks "
-                + "where `ticker` = ?;";
+        final String sql = "select s.stock_id, s.`name`, s.`ticker`, s.asset_type, s.industry, "
+                + "se.stock_exchange_id, se.`name`, se.`code`, se.timezone, "
+                + "c.country_id, c.`name`, c.`code`, "
+                + "cu.currency_id, cu.`name`, cu.`code`, cu.value_to_usd "
+                + "from stock_exchange se "
+                + "inner join stocks s on se.stock_exchange_id = s.stock_exchange_id "
+                + "inner join countries c on s.country_id = c.country_id "
+                + "inner join currencies cu on c.currency_id = cu.currency_id "
+                + " where s.`ticker` = ?;";
 
         return jdbcTemplate.query(sql, new StockMapper(), ticker)
                 .stream()
