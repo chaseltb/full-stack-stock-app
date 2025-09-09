a09c2d27a7668d794b5d74ea7418044b078341ef
stokesm5 <stokesm5@tcnj.edu>
2025-09-09 18:58:43 -0400

    find methods implemented, began validate method

--- lava-donut/src/main/java/learn/lavadonut/domain/StockService.java
+++ lava-donut/src/main/java/learn/lavadonut/domain/StockService.java
@@ -1,11 +1,44 @@
 package learn.lavadonut.domain;
 
 import learn.lavadonut.data.StockRepository;
+import learn.lavadonut.models.Stock;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StockService {
     private final StockRepository repository;
 
     public StockService(StockRepository repository) { this.repository = repository; }
+
+    public List<Stock> findAll(){ return repository.findAll(); }
+
+    public Result<List<Stock>> getStocksByIndustry(String industry) {
+        Result<List<Stock>> result = new Result<>();
+
+        if(Validations.isNullOrBlank(industry)){
+            result.addMessage("industry must not be null or blank!", ResultType.INVALID);
+            return result;
+        }
+
+        result.setPayload(repository.getStocksByIndustry(industry));
+
+        return result;
+    }
+
+    public Stock findById(int stockId){ return repository.findById(stockId); }
+
+    private Result<Stock> validate(Stock stock){
+        Result<Stock> result = new Result<>();
+
+        if(stock == null){
+            result.addMessage("stock cannot be null!", ResultType.INVALID);
+            return result;
+        }
+
+
+
+        return null;
+    }
 }
