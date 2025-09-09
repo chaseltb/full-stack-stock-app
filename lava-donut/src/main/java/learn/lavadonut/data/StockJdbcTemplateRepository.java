cdb86f3c862b9f0a97921868761b24e8c577e8af
stokesm5 <stokesm5@tcnj.edu>
2025-09-09 16:48:56 -0400

    StockJdbcRepo init and findAll method implemented

---
+++ lava-donut/src/main/java/learn/lavadonut/data/StockJdbcTemplateRepository.java
@@ -0,0 +1,51 @@
+package learn.lavadonut.data;
+
+import learn.lavadonut.data.mappers.StockMapper;
+import learn.lavadonut.models.Stock;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class StockJdbcTemplateRepository implements StockRepository{
+    private final JdbcTemplate jdbcTemplate;
+
+    public StockJdbcTemplateRepository(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
+
+    //TODO: IF ERROR, RE-EXAMINE STOCK MAPPER
+    @Override
+    public List<Stock> findAll() {
+        final String sql = "select stock_id, `name`, `ticker`, asset_type, industry, stock_exchange_id, country_id "
+                + "from stocks limit 1000;";
+        return jdbcTemplate.query(sql, new StockMapper());
+    }
+
+    @Override
+    public List<Stock> getStocksByIndustry(String industry) {
+        return List.of();
+    }
+
+    @Override
+    public Stock findById(int stockId) {
+        return null;
+    }
+
+    @Override
+    public Stock findByTicker(String ticker) {
+        return null;
+    }
+
+    @Override
+    public Stock add(Stock stock) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Stock stock) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int stockId) {
+        return false;
+    }
+}
