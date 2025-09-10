c355782d8b05e7b2ad7fa0ffa2f5ef796725ff0e
stokesm5 <stokesm5@tcnj.edu>
2025-09-10 14:30:39 -0400

    StockJdbcRepo test init

---
+++ lava-donut/src/test/java/learn/lavadonut/data/StockJdbcTemplateRepositoryTest.java
@@ -0,0 +1,24 @@
+package learn.lavadonut.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class StockJdbcTemplateRepositoryTest {
+
+    final static int NEXT_ID = 10;
+
+    @Autowired
+    StockJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() { knownGoodState.set(); }
+
+
+}
