929ae92bb9d660526027a5b94eae07b94b55ea01
DerRuck <tyler.ruckstaetter@gmail.com>
2025-09-10 13:43:04 -0500

    portfolio controller (still some potential todos)

--- lava-donut/src/main/java/learn/lavadonut/controllers/PortfolioController.java
+++ lava-donut/src/main/java/learn/lavadonut/controllers/PortfolioController.java
@@ -1,15 +1,20 @@
 package learn.lavadonut.controllers;
 
 import learn.lavadonut.domain.PortfolioService;
+import learn.lavadonut.domain.Result;
 import learn.lavadonut.models.AccountType;
 import learn.lavadonut.models.Portfolio;
 import learn.lavadonut.models.Stock;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/portfolio")
 public class PortfolioController {
 
     private final PortfolioService service;
@@ -18,31 +23,45 @@ public class PortfolioController {
         this.service = service;
     }
 
-    @GetMapping
+    @GetMapping("/{userId}")
     public ResponseEntity<Portfolio> findByUserId(@PathVariable int userId) {
-
-        return null;
+        Portfolio portfolio = service.findByUserId(userId);
+        if(portfolio == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(portfolio);
     }
 
-    @GetMapping
+    @GetMapping("/{userId}/stocks")
     public ResponseEntity<List<Stock>> findAllStocksInPortfolio(@PathVariable int userId) {
-
-        return null;
+        List<Stock> stocks = service.findAllOwnedStocksInPortfolio(userId);
+        if (stocks == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(stocks);
     }
-    @GetMapping
+    @GetMapping("/{userId}/value")
     public ResponseEntity<BigDecimal> getPortfolioValue(@PathVariable int userId, @RequestParam String date) {
-
-        return null;
+        Result<BigDecimal> result = service.getPortfolioValue(userId, date);
+        if (result.isSuccess()) {
+            return ResponseEntity.ok(result.getPayload());
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    @GetMapping
+    @GetMapping("/userId/cost_basis")
     public ResponseEntity<Void> getCostBasisOnDividend(@PathVariable int userId, @RequestBody BigDecimal dividend) {
 
+        //TODO BigDecimal costBasis = service.updateCostBasisOnDividend(userId, dividend).getPayload();
         return null;
     }
-    @PutMapping
+    @PutMapping("/{userId}")
     public ResponseEntity<Portfolio> updateAccountType(@PathVariable int userId, @RequestParam AccountType accountType) {
 
-        return null;
+        Result<Portfolio> result = service.updateAccountType(userId, accountType);
+        if (result.isSuccess()) {
+            return ResponseEntity.ok(result.getPayload());
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     //TODO decide if we want this functionality
 //    public ResponseEntity<Void> sellStockFromPortfolio(@PathVariable int userId, @PathVariable int stockId) {
