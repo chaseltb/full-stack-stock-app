a3cf76a606f3e8aa85af8e6cfbbb76c15ad45687
stokesm5 <stokesm5@tcnj.edu>
2025-09-10 16:33:21 -0400

    makeStock helper method implemented

--- lava-donut/src/test/java/learn/lavadonut/domain/StockServiceTest.java
+++ lava-donut/src/test/java/learn/lavadonut/domain/StockServiceTest.java
@@ -1,9 +1,16 @@
 package learn.lavadonut.domain;
 
 import learn.lavadonut.data.StockRepository;
+import learn.lavadonut.models.AssetType;
+import learn.lavadonut.models.Country;
+import learn.lavadonut.models.Stock;
+import learn.lavadonut.models.StockExchange;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +22,27 @@ class StockServiceTest {
 
     @MockBean
     StockRepository repository;
-    
+
+
+    Stock makeStock() {
+        Stock stock = new Stock();
+
+        stock.setId(1);
+        stock.setName("TEST STOCK NAME");
+        stock.setTicker("TEST-STOCK-TICKER");
+        stock.setAssetType(AssetType.STOCK);
+        stock.setIndustry("TEST-INDUSTRY");
+        stock.setCurrentPrice(BigDecimal.valueOf(10.50));
+
+        Country country = new Country();
+        country.setId(2);
+        stock.setCountry(country);
+
+        StockExchange stockExchange = new StockExchange();
+        stockExchange.setId(2);
+        stock.setStockExchange(stockExchange);
+
+        return stock;
+    }
+
 }
