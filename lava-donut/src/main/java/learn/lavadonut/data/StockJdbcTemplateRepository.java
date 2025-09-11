87d8e1d3da905b47aecc38b5e5b7fdc28f3d52e6
stokesm5 <stokesm5@tcnj.edu>
2025-09-11 14:09:33 -0400

    shouldFindByIndustry test passing, changed sql in StockRepo to differentiate fields by alias

--- lava-donut/src/main/java/learn/lavadonut/data/StockJdbcTemplateRepository.java
+++ lava-donut/src/main/java/learn/lavadonut/data/StockJdbcTemplateRepository.java
@@ -34,17 +34,17 @@ public class StockJdbcTemplateRepository implements StockRepository{
 
     @Override
     public List<Stock> getStocksByIndustry(String industry) {
-        final String sql = "select s.stock_id, s.`name`, s.`ticker`, s.asset_type, s.industry, s.current_price, "
-                + "se.stock_exchange_id, se.`name`, se.`code`, se.timezone, "
-                + "c.country_id, c.`name`, c.`code`, "
-                + "cu.currency_id, cu.`name`, cu.`code`, cu.value_to_usd "
+        final String sql = "select s.stock_id, s.`name` as stock_name, s.`ticker`, s.asset_type, s.industry, s.current_price, "
+                + "se.stock_exchange_id, se.`name` as exchange_name, se.`code` as exchange_code, se.timezone, "
+                + "c.country_id, c.`name` as country_name, c.`code` as country_code, "
+                + "cu.currency_id, cu.`name` as currency_name, cu.`code` as currency_code, cu.value_to_usd "
                 + "from stock_exchange se "
                 + "inner join stocks s on se.stock_exchange_id = s.stock_exchange_id "
                 + "inner join countries c on s.country_id = c.country_id "
                 + "inner join currencies cu on c.currency_id = cu.currency_id "
-                + " where s.industry = ? "
+                + "where s.industry = ? "
                 + "limit 1000;";
-        return jdbcTemplate.query(sql, new StockMapper());
+        return jdbcTemplate.query(sql, new StockMapper(), industry);
     }
 
     @Override
@@ -57,7 +57,7 @@ public class StockJdbcTemplateRepository implements StockRepository{
                 + "inner join stocks s on se.stock_exchange_id = s.stock_exchange_id "
                 + "inner join countries c on s.country_id = c.country_id "
                 + "inner join currencies cu on c.currency_id = cu.currency_id "
-                + " where s.stock_id = ?;";
+                + "where s.stock_id = ?;";
 
         return jdbcTemplate.query(sql, new StockMapper(), stockId)
                 .stream()
