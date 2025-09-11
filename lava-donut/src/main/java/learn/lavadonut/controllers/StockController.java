82500b0ed0c3a25ad0b8bfceb534dda0e44efda4
stokesm5 <stokesm5@tcnj.edu>
2025-09-11 11:44:59 -0400

    final touches

--- lava-donut/src/main/java/learn/lavadonut/controllers/StockController.java
+++ lava-donut/src/main/java/learn/lavadonut/controllers/StockController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//TODO: turn into REST controller
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/stocks")
