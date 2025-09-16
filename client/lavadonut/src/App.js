390dd7fb69f72175686c2af7329bccbe87a2c0bd
stokesm5 <stokesm5@tcnj.edu>
2025-09-16 11:37:02 -0400

    merge should work

--- client/lavadonut/src/App.js
+++ client/lavadonut/src/App.js
@@ -1,17 +1,35 @@
-import AuthPage from "./AuthPage";
-import OrderHistory from "./OrderHistory";
-import { Routes, BrowserRouter as Router, Route, Navigate } from "react-router-dom";
 import React from "react";
+
+import AuthPage from "./AuthPage";
+import NotFound from "./NotFound";
+import CountryList from "./CountryList";
+import RequireAuth from "./RequireAuth";
+import CountryForm from "./CountryForm";
+import HomeScreen from "./HomeScreen";
+import Portfolios from "./Portfolios";
+import Stock from "./Stock";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<AuthPage />}/>
-          <Route path="/OrderHistory/:name/:id" element={<OrderHistory />}/>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        {/* Auth route */}
+        <Route path="/" element={<AuthPage />} />
+
+        {/* Protected routes */}
+        <Route path="/countries"
+          element={<RequireAuth> <CountryList /> </RequireAuth>}/>
+        <Route path="/country/add" element={<RequireAuth> <CountryForm /> </RequireAuth>} />
+        <Route path="/country/edit/:id" element={<RequireAuth> <CountryForm /> </RequireAuth>}/>
+        <Route path="/home" element={<RequireAuth> <HomeScreen /> </RequireAuth>}/>
+        <Route path="/stock" element={<RequireAuth> <Stock /> </RequireAuth>}/>
+        <Route path="/portfolios" element={<RequireAuth> <Portfolios /> </RequireAuth>}/>
+
+        {/* Catch-all */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
