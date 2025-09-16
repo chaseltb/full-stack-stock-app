9ddc441516c1d947d91cbb703ce22ac65edd2f99
stokesm5 <stokesm5@tcnj.edu>
2025-09-16 11:17:01 -0400

    resolve merge conflicts

--- client/lavadonut/src/App.js
+++ client/lavadonut/src/App.js
@@ -1,18 +1,6 @@
-import AuthPage from "./AuthPage";
-import OrderHistory from "./OrderHistory";
-import { Routes, BrowserRouter as Router, Route, Navigate } from "react-router-dom";
-import React from "react";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<AuthPage />}/>
-          <Route path="/OrderHistory/:name/:id" element={<OrderHistory />}/>
-        </Routes>
-      </Router>
-    </>
   );
 }
 
