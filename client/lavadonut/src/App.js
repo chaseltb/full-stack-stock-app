db61a347390483dce5b4a26b131c3685d0c5c5b2
stokesm5 <stokesm5@tcnj.edu>
2025-09-16 11:34:47 -0400

    redoing

---
+++ client/lavadonut/src/App.js
@@ -0,0 +1,17 @@
+import AuthPage from "./AuthPage";
+import OrderHistory from "./OrderHistory";
+import { Routes, BrowserRouter as Router, Route, Navigate } from "react-router-dom";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AuthPage />}/>
+          <Route path="/OrderHistory/:name/:id" element={<OrderHistory />}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
--- client/lavadonut/src/OrderHistory.js
+++ client/lavadonut/src/OrderHistory.js
@@ -61,7 +61,7 @@ function OrderHistory() {
   };
 
   const orderHistory = {
-    labels: userStocks.map(stock => stock.name), // check if order and stock are same before mapping
+    labels: userStocks.map(stock => stock.name), // check if order and stock are same before
     datasets: [
       {
         label: "Cost",
