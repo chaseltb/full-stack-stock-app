afc653f92108472af0ced1b8574a40ca0a53806d
chaseltb <chasethethompson@icloud.com>
2025-09-15 22:49:33 -0400

    portfolios jsx progress and stock start on frontend

--- client/lavadonut/src/Portfolios.js
+++ client/lavadonut/src/Portfolios.js
@@ -23,8 +23,51 @@ function Portfolios() {
     };
 
     return (
-        <>
-        </>
+        <Container maxWidth="lg">
+            <Paper elevation={4} sx={{ mt: 4, p: 4, borderRadius: 10 }}>
+                <Box sx={{ display: "flex", justifyContent: "space-between", alignItem: "center", mb: 4}}>
+                    <Typography variant="h2">
+                        Portfolios
+                    </Typography>
+                    <Button variant="contained" sx={{ borderRadius: 5 }}>
+                        Add Portfolio
+                    </Button>
+                </Box>
+
+                {error && <Alert severity="error" sx={{ mb: 4 }}>
+                    {error}
+                </Alert>}
+
+                {/* list of portfolios */}
+                <Box sx={{ display: "grid", gap: 3}}>
+                    {portfolios.map((portfolio) => (
+                        <Paper key={portfolio.id} elevation={2} sx={{ p: 2, borderRadius: 10}}>
+                            <Box sx={{ p: 2, mb: 2}}>
+                                <Typography variant="body1">
+                                    {portfolio.name}
+                                </Typography>
+                                <Typography variant="body1">
+                                    {portfolio.type}
+                                </Typography>
+                            </Box>
+
+                            <Box sx={{ mb: 4 }}>
+                                <Typography variant="body1">
+                                    Total Shares: 
+                                </Typography>
+                                <Typography variant="body1">
+                                    Total Value:
+                                </Typography>
+                                <Typography variant="body1" 
+                                    color={portfolio.unrealizedGainOrLoss >= 0 ? "success" : "error"}>
+                                        Unrealized Gain/Loss: 
+                                </Typography>
+                            </Box>
+                        </Paper>
+                    ))}
+                </Box>
+            </Paper>
+        </Container>
     );
 
 }
