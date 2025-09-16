146427dbce88183ada79488d59c046b3da966870
tomiwaadewolu <tomi.aj91@gmail.com>
2025-09-15 20:08:02 -0500

    Resolving conflicts

--- client/lavadonut/src/App.js
+++ client/lavadonut/src/App.js
@@ -1,28 +1,31 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AuthPage from "./AuthPage";
+import HomeScreen from "./HomeScreen";
+import { Routes, BrowserRouter as Router, Route, Navigate } from "react-router-dom";
+import React from "react";
+
 import NotFound from "./NotFound";
 import CountryList from "./CountryList";
 import RequireAuth from "./RequireAuth";
 import CountryForm from "./CountryForm";
-import OrderList from "./OrderList";
-import OrderForm from "./OrderForm";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<AuthPage />}/>
-          <Route path="/countries" element={<CountryList />}/>
-          <Route path="/country/add" element={<RequireAuth> <CountryForm /> </RequireAuth>}/>
-          <Route path="/country/edit/:id" element={<RequireAuth> <CountryForm /> </RequireAuth>}/>
-          <Route path="/orders" element={<RequireAuth> <OrderList /> </RequireAuth>}/>
-          <Route path="/order/add" element={ <OrderForm /> }/>
-          <Route path="/order/edit/:id" element={<RequireAuth> <OrderForm /> </RequireAuth>}/>
-          <Route path="*" element={<NotFound />}/>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        {/* Auth route */}
+        <Route path="/" element={<AuthPage />} />
+
+        {/* Protected routes */}
+        <Route path="/countries"
+          element={<RequireAuth> <CountryList /> </RequireAuth>}/>
+        <Route path="/country/add" element={<RequireAuth> <CountryForm /> </RequireAuth>} />
+        <Route path="/country/edit/:id" element={<RequireAuth> <CountryForm /> </RequireAuth>}/>
+        <Route path="/home" element={<RequireAuth> <HomeScreen /> </RequireAuth>}/>
+
+        {/* Catch-all */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
